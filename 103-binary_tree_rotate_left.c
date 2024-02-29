b8579c6ff03bb517f8d7e6d92fc7da9dea7a4e7e
minasamirr <1701538@eng.asu.edu.eg>
2024-02-29 14:44:15 -0800

    Task

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,25 +8,18 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	binary_tree_t *new_root;
+	binary_tree_t *new_root, *temp;
 
 	if (tree == NULL || tree->right == NULL)
 		return (NULL);
 
-	new_root = tree->right;
-	tree->right = new_root->left;
-	if (new_root->left != NULL)
-		new_root->left->parent = tree;
+	temp = tree->left->right;
+	new_root = tree->left;
 	new_root->parent = tree->parent;
-	if (tree->parent != NULL)
-	{
-		if (tree->parent->left == tree)
-			tree->parent->left = new_root;
-		else
-			tree->parent->right = new_root;
-	}
+	new_root->right = tree;
 	tree->parent = new_root;
-	new_root->left = tree;
-
+	tree->left = temp;
+	if (temp)
+		temp->parent = tree;
 	return (new_root);
 }
