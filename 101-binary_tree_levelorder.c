8a8140e8055c6d3e5423bf2caf07174411069da6
minasamirr <1701538@eng.asu.edu.eg>
2024-02-29 13:32:52 -0800

    task

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -10,21 +10,21 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        if (tree)
-        {
-                int left = 0, right = 0;
+	if (tree)
+	{
+		int left = 0, right = 0;
 
-                if (tree->right)
-                        right = 1 + binary_tree_height(tree->right);
-                if (tree->left)
-                        left = 1 + binary_tree_height(tree->left);
-                if (left > right)
-                        return (left);
-                else
-                        return (right);
-        }
-        else
-                return (0);
+		if (tree->right)
+			right = 1 + binary_tree_height(tree->right);
+		if (tree->left)
+			left = 1 + binary_tree_height(tree->left);
+		if (left > right)
+			return (left);
+		else
+			return (right);
+	}
+	else
+		return (0);
 }
 
 /**
