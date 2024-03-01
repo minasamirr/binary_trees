9ae7652200913a9e55d6f2f4e4f64bd1a1122a8f
minasamirr <1701538@eng.asu.edu.eg>
2024-03-01 04:02:37 -0800

    fix

--- binary_trees.h
+++ binary_trees.h
@@ -16,10 +16,10 @@
  */
 struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -37,9 +37,6 @@ typedef struct levelorder_queue_s
 	binary_tree_t *node;
 	struct levelorder_queue_s *next;
 } levelorder_queue_t;
-
-/* Structure for Binary Search Tree Node */
-typedef struct binary_tree_s bst_t;
 
 /* Printing helper function */
 void binary_tree_print(const binary_tree_t *);
@@ -63,18 +60,15 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, 
 	const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
-int binary_tree_is_complete_util(const binary_tree_t *tree, size_t index,
-		size_t size, size_t height);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
-void binary_tree_delete(binary_tree_t *tree);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
 int binary_tree_is_avl(const binary_tree_t *tree);
